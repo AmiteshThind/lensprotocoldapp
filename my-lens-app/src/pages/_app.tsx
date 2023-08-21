4c0d751bc6c69da728fd476458ea9f251794ec4f
Amitesh Thind <thindamitesh@gmail.com>
2023-08-20 21:59:01 -0400

    added authentication

---
+++ my-lens-app/src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import "dotenv/config";
+export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
+  return (
+    <ThirdwebProvider
+      activeChain={ChainId.Mumbai}
+      clientId={process.env.CLIENT_ID}
+    >
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThirdwebProvider>
+  );
+}
---
+++ my-lens-app/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import useLogin from "../lib/auth/useLogin";
+import SignInButton from "../Components/SignInButton";
+
+export default function Home() {
+  return <SignInButton />;
+}
