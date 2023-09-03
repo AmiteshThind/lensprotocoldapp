7668c6ae439c4c717abf3e8831310865cf182dd3
Amitesh Thind <thindamitesh@gmail.com>
2023-09-03 12:33:49 -0400

    go back from user profile page functionality

--- my-lens-app/src/pages/profile/[id].tsx
+++ my-lens-app/src/pages/profile/[id].tsx
@@ -1,6 +1,18 @@
+// see selected users profile pic
+// see user bio
+// see user handle
+// see user followers
+// see user following
+// see if user follows me
+// see if i follow user
+// opton to follow or unfollow user
+// see list of Wizzies User has created and have option to comment  on them/ interact with them just like in the feed
+
 import { useProfileQuery } from "@/src/graphql/generated";
 import React from "react";
 import { useRouter } from "next/router";
+import { AiOutlineLeftCircle } from "react-icons/ai";
+import Link from "next/link";
 type Props = {};
 
 const ProfilePage = (props: Props) => {
@@ -15,15 +27,19 @@ const ProfilePage = (props: Props) => {
 
   console.log(profileData);
 
+  //row 1 - back button
+  // row 2 profile pic, bio, follow button , stats
+  // row 3 - list of wizzes in feed post format (sorted by date or something (add additional sort functionality later (post MVP)))
+
   return (
-    <div>
-      <div>
-        {/*cover image*/}
-        {/*Profile picture*/}
-        {/*profile name*/}
-        {/*profile handle*/}
-        {/*profile description*/}
+    <div className="flex flex-col mt-20 p-10 ">
+      <div className="w-full rounded-full">
+        <Link href={"/"}>
+          <AiOutlineLeftCircle className="text-emerald-300 h-8 w-8" />
+        </Link>
       </div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
