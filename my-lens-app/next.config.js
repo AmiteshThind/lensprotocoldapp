18ce8ce63b911dd4b01214fd95fa82227a9d7444
Amitesh Thind <thindamitesh@gmail.com>
2023-10-05 14:22:41 -0400

    app id config

--- my-lens-app/next.config.js
+++ my-lens-app/next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     CLIENT_ID: process.env.CLIENT_ID,
     NEXT_PUBLIC_STUDIO_API_KEY: process.env.NEXT_PUBLIC_STUDIO_API_KEY,
   },
+  transpilePackages: ["@lens-protocol"],
 };
 
 module.exports = nextConfig;
