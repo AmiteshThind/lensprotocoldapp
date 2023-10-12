81036a04c7f76b8df49ed04ec46fca9181499e42
Amitesh Thind <thindamitesh@gmail.com>
2023-10-11 23:51:23 -0400

    updated landing page stlying

--- my-lens-app/src/pages/landing.tsx
+++ my-lens-app/src/pages/landing.tsx
@@ -17,7 +17,7 @@ function landing({}: Props) {
             stiffness: 800,
             damping: 10,
           }}
-          className="  mt-16 font-bold text-neutral-800"
+          className="text-neutral-100 duration-100  mt-16 font-bold "
         >
           WisDao
         </motion.div>
@@ -32,14 +32,14 @@ function landing({}: Props) {
             stiffness: 800,
             damping: 10,
           }}
-          className=" w-2/3 md:w-1/3 mt-16 text-center text-neutral-700 font-semibold"
+          className="duration-100 w-2/3 md:w-1/3 mt-16 text-center text-neutral-700 font-semibold"
         >
           A decentralzied social media application focused on exchanging
           learnings from personal experiences to empower growth, learning, unity
           and data ownership among users of the platform.
         </motion.div>
         <motion.button
-          initial={{ scale: 0.1 }}
+          initial={{ scale: 0 }}
           animate={{ scale: 1 }}
           transition={{
             delay: 2,
@@ -48,7 +48,7 @@ function landing({}: Props) {
             stiffness: 400,
             damping: 10,
           }}
-          className="cursor hover:scale-110 duration-100 p-3 rounded-lg border-none mt-16 bg-teal-600 font-semibold text-neutral-800 mx-2"
+          className="cursor-pointer hover:bg-emerald-500   duration-100 p-3 rounded-lg border-none mt-16 bg-neutral-700 font-semibold text-white mx-2"
         >
           <Link href={"/home"}>Enter App</Link>
         </motion.button>
