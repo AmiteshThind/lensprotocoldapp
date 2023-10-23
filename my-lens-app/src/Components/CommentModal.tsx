8b28e9be17309b53b08eb5590a5fd453cc248c2d
Amitesh Thind <thindamitesh@gmail.com>
2023-10-23 12:34:06 -0400

    fixed error message display and comment creation functionality

--- my-lens-app/src/Components/CommentModal.tsx
+++ my-lens-app/src/Components/CommentModal.tsx
@@ -10,14 +10,16 @@ import { MediaRenderer, Web3Button } from "@thirdweb-dev/react";
 import { LENS_CONTRACT_ADDRESS, LENS_CONTRACT_ABI } from "../const/contracts";
 import { useCreatePost } from "../lib/auth/usePost";
 import { ExplorePublicationsQuery } from "../graphql/generated";
+import { useCreateComment } from "../lib/auth/useCreateComment";
 interface Props {
   publication: ExplorePublicationsQuery["explorePublications"]["items"][0];
 }
 
 const CommentModal = ({ publication }: Props) => {
-  const { mutateAsync: createPost } = useCreatePost();
+  const { mutateAsync: creatComment } = useCreateComment();
   const [newWizzComment, setNewWizzComment] = useState<WizzComment>({
     text: "",
+    publicationId: publication.id,
   });
 
   const [errorMessages, setErrorMessages] = useState<WizzCommentError>({
@@ -37,87 +39,116 @@ const CommentModal = ({ publication }: Props) => {
   }
 
   return (
-    <div className="w-full">
-      <form
-        onSubmit={createNewWizzComment}
-        className="w-full border border-neutral-800 rounded-xl  "
-      >
-        <div>
-          {errorMessages.textError ? (
-            <span className="text-red-500 mx-3 my-2 flex">
-              <BiErrorCircle className="h-6 w-4" />
-              <span className="ml-1">{errorMessages.textError}</span>
-            </span>
-          ) : (
-            <></>
-          )}
-        </div>
-
+    <dialog id={"my_modal_4" + publication.id} className="modal">
+      <div className="modal-box p-8">
         <div className="w-full">
-          <div className="w-full flex my-5 ">
-            <div className="avatar h-14  ">
-              <div className=" mx-3 rounded-full">
-                <MediaRenderer
-                  width="100%"
-                  height="100%"
-                  src={
-                    // @ts-ignore - the type does exist
-                    publication?.profile?.picture?.original?.url ||
-                    "https://soccerpointeclaire.com/wp-content/uploads/2021/06/default-profile-pic-e1513291410505.jpg"
-                  }
-                />
-              </div>
-            </div>
-            <p className=" textarea w-full mx-3">
-              {publication?.metadata?.description}
-            </p>
-          </div>
-          <div className="flex">
-            <div className="avatar  h-14 ">
-              <div className=" mx-3 rounded-full">
-                <MediaRenderer
-                  width="100%"
-                  height="100%"
-                  src={
-                    // @ts-ignore - the type does exist
-                    publication?.profile?.picture?.original?.url ||
-                    "https://soccerpointeclaire.com/wp-content/uploads/2021/06/default-profile-pic-e1513291410505.jpg"
-                  }
-                />
-              </div>
-            </div>
-            <div className="mx-3 w-full">
-              <textarea
-                onChange={(e) => {
-                  setNewWizzComment({
-                    ...newWizzComment,
-                    text: e.target.value,
+          <form
+            onSubmit={createNewWizzComment}
+            className="w-full border border-neutral-800 rounded-xl  "
+          >
+            <form method="dialog">
+              <button
+                onClick={(e) => {
+                  setNewWizzComment({ ...newWizzComment, text: "" });
+                  setErrorMessages({
+                    ...errorMessages,
+                    textError: "",
                   });
                 }}
-                className="textarea textarea-bordered border-2 h-24 w-full text-white"
-                placeholder="Post Reply"
-              ></textarea>
+                className="btn btn-xs mx-1 my-1 btn-circle btn-ghost absolute right-1 top-1"
+              >
+                ✕
+              </button>
+            </form>
+            <div>
+              {errorMessages.textError ? (
+                <span className="text-red-500 mx-3 my-2 flex">
+                  <BiErrorCircle className="h-6 w-4" />
+                  <span className="ml-1">{errorMessages.textError}</span>
+                </span>
+              ) : (
+                <></>
+              )}
             </div>
-          </div>
-          <div className="w-full my-5 flex justify-center">
-            <Web3Button
-              contractAddress={LENS_CONTRACT_ADDRESS}
-              contractAbi={LENS_CONTRACT_ABI}
-              // action={async () => {
-              //   await (newWizz).then(() => {
-              //     setNewWizzComment({
-              //     text:""
-              //     });
-              //   });
-              // }}
-              className="btn-success rounded-3xl mt-5 btn btn-wide"
-            >
-              Reply
-            </Web3Button>
-          </div>
+
+            <div className="w-full ">
+              <div className="w-full flex my-5   border-neutral-600  ">
+                <div className="avatar h-14    ">
+                  <div className=" mx-3 rounded-full">
+                    <MediaRenderer
+                      width="100%"
+                      height="100%"
+                      src={
+                        // @ts-ignore - the type does exist
+                        publication?.profile?.picture?.original?.url ||
+                        "https://soccerpointeclaire.com/wp-content/uploads/2021/06/default-profile-pic-e1513291410505.jpg"
+                      }
+                    />
+                  </div>
+                </div>
+                <p className="  textarea w-full mx-3">
+                  {publication?.metadata?.description}
+                </p>
+              </div>
+              <div className="flex">
+                <div className="avatar  h-14 ">
+                  <div className=" mx-3 rounded-full">
+                    <MediaRenderer
+                      width="100%"
+                      height="100%"
+                      src={
+                        // @ts-ignore - the type does exist
+                        publication?.profile?.picture?.original?.url ||
+                        "https://soccerpointeclaire.com/wp-content/uploads/2021/06/default-profile-pic-e1513291410505.jpg"
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="mx-3 w-full">
+                  <textarea
+                    onChange={(e) => {
+                      setNewWizzComment({
+                        ...newWizzComment,
+                        text: e.target.value,
+                      });
+                    }}
+                    className="textarea textarea-bordered border-2 h-24 w-full text-white"
+                    placeholder="Post Reply"
+                    value={newWizzComment.text}
+                  ></textarea>
+                </div>
+              </div>
+              <div className="w-full my-5 flex justify-center">
+                <Web3Button
+                  contractAddress={LENS_CONTRACT_ADDRESS}
+                  contractAbi={LENS_CONTRACT_ABI}
+                  action={async () => {
+                    console.log("called reply");
+                    if (newWizzComment.text != "") {
+                      await creatComment(newWizzComment).then(() => {
+                        setNewWizzComment({ ...newWizzComment, text: "" });
+
+                        document
+                          .getElementById("my_modal_4" + publication.id)
+                          .close();
+                      });
+                    } else {
+                      setErrorMessages({
+                        ...errorMessages,
+                        textError: "Comment must contain text",
+                      });
+                    }
+                  }}
+                  className="btn-success rounded-3xl mt-5 btn btn-wide"
+                >
+                  Reply
+                </Web3Button>
+              </div>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      </div>
+    </dialog>
   );
 };
 
